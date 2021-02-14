20bd3f45bbb9e50302f7fb1fbd9ba260bd3726df
Francois Gervais <francoisgervais@gmail.com>
2021-02-14 18:46:45 -0500

    Print subvolumes like in the example

--- btrfs-tool.py
+++ btrfs-tool.py
@@ -1,6 +1,28 @@
 import btrfs
 
 
+def subvolumes_inside(fs, parent_tree):
+    min_key = btrfs.ctree.Key(parent_tree, btrfs.ctree.ROOT_REF_KEY, 0)
+    max_key = btrfs.ctree.Key(parent_tree, btrfs.ctree.ROOT_REF_KEY + 1, 0) - 1
+    for header, data in btrfs.ioctl.search_v2(fs.fd, 1, min_key, max_key):
+        ref = btrfs.ctree.RootRef(header, data)
+        path = (btrfs.ioctl.ino_lookup(fs.fd, ref.parent_tree, ref.dirid).name_bytes +
+                ref.name).decode()
+        yield ref.tree, path
+
+
+def print_subvolumes_inside(fs, parent_tree=btrfs.ctree.FS_TREE_OBJECTID, parent_path=""):
+    for tree, path in subvolumes_inside(fs, parent_tree):
+        sub_path = "{}/{}".format(parent_path, path)
+        print("ID {} parent {} path {}".format(tree, parent_tree, sub_path))
+        print_subvolumes_inside(fs, tree, sub_path)
+
+
 with btrfs.FileSystem('/home/fgervais/btrfs') as fs:
-    for space in fs.space_info():
-        print(space)
+    # for subvol in fs.subvolumes():
+    #     path = (btrfs.ioctl.ino_lookup(fs.fd, ref.parent_tree, ref.dirid).name_bytes +
+    #             ref.name).decode()
+    #     print(type(subvol))
+    #     break
+
+    print_subvolumes_inside(fs)
